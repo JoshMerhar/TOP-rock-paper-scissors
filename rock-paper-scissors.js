3be8148831b571be8fe0e69408bf6b7060f6ad70
Josh Merhar <122338744+JoshMerhar@users.noreply.github.com>
2023-06-01 12:10:48 -0400

    Fix game function and improve playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,16 +1,19 @@
 let choices = ["rock", "paper", "scissors"];
 
-function getComputerChoice(choices) {
+function getComputerChoice() {
     let selection = choices[(Math.floor(Math.random() * choices.length))];
     return selection;
 }
 
 let playerSelection;
-const computerSelection = getComputerChoice(choices);
+let computerSelection;
 let result;
 
 function playRound(computerSelection, playerSelection) {
-    playerSelection = prompt("Choose your fighter: ").toLowerCase();
+    while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+        playerSelection = prompt("Choose your fighter: ").toLowerCase();
+    }
+    computerSelection = getComputerChoice(choices);
     if (computerSelection === playerSelection) {
         return result = "It's a draw!";
     } else if ((computerSelection === "rock" && playerSelection != "paper") 
@@ -22,23 +25,26 @@ function playRound(computerSelection, playerSelection) {
     }
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
 function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
     for (let i = 0; i < 5; i++) {
-        playRound(computerSelection, playerSelection);
+        result = playRound(computerSelection, playerSelection);
         if (result === "The computer wins!") {
             computerScore++;
-            i++;
         } else if (result === "You win!") {
             playerScore++;
-            i++;
-        } else if (result === "It's a draw!") {
-            i++;
         }
         console.log(result);
     }
+    if (playerScore > computerScore) {
+        console.log("You beat the machine!");
+    } else if (playerScore < computerScore) {
+        console.log("The computer wins!");
+    } else {
+        console.log("It's a draw!");
+    }
 }
 
-console.log(game(computerSelection, playerSelection));
+console.log(game());
